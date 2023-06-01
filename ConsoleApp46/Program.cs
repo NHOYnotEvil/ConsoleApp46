6772f20bcdd5917c5ec909eca58fbefbd7395697
NHOY <NHOY@2210DR000919>
2023-06-02 00:35:21 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp46/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace ConsoleApp46
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string commandCloseProgram = "exit";
+            string userInput;
+
+            while (true)
+            {
+                Console.WriteLine("Введите команду - " + "\"" + commandCloseProgram + "\"" + " для закрытия программы.");
+                userInput = Console.ReadLine();
+
+                if (userInput == commandCloseProgram)
+                {
+                    break;
+                }
+                else if (userInput != commandCloseProgram)
+                {
+                    Console.WriteLine("Попробуйте снова.");
+                }
+            }
+        }
+    }
+}
